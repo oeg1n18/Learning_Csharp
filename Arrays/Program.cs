f2e5beb0926887d6c11c38f884d94e12b3e3d57a
Oliver Grainge <olivergrainge@Olivers-MacBook-Pro.local>
2021-08-20 09:02:58 +0100

    first commit

---
+++ Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] balance = new double[10]; 
+            balance[0] = 4500.0;
+
+            int [] marks = new int[5] {99, 98, 97, 92, 91};
+
+            // to loop through each element 
+            foreach (int j in marks) {
+                Console.WriteLine(j);
+            }
+        }
+    }
+}
